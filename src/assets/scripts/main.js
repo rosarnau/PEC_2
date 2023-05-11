07abbbaf45d4afa0cdcc4c6b7cec8fbdadcfdf37
rosarnau <116513309+rosarnau@users.noreply.github.com>
2023-05-12 01:47:59 +0200

    first commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,6 +2,7 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
+import * as bootstrap from 'bootstrap';
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
